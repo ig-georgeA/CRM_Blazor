d0194640a9aaf49855eaaaac2191441c5370e949
George Abraham <41584300+ig-georgeA@users.noreply.github.com>
2022-11-08 13:27:40 -0500

    Buttons updated with bg and fill colors

--- TestCustomer Relationship Management/Pages/TestAccountSample.cs
+++ TestCustomer Relationship Management/Pages/TestAccountSample.cs
@@ -12,9 +12,9 @@ namespace TestCustomer_Relationship_Management
 			using var ctx = new TestContext();
 			ctx.JSInterop.Mode = JSRuntimeMode.Loose;
 			ctx.Services.AddIgniteUIBlazor(
+				typeof(IgniteUI.Blazor.Controls.IgbTabsModule),
 				typeof(IgniteUI.Blazor.Controls.IgbButtonModule),
 				typeof(IgniteUI.Blazor.Controls.IgbRippleModule),
-				typeof(IgniteUI.Blazor.Controls.IgbTabsModule),
 				typeof(IgniteUI.Blazor.Controls.IgbExpansionPanelModule),
 				typeof(IgniteUI.Blazor.Controls.IgbListModule),
 				typeof(IgniteUI.Blazor.Controls.IgbAvatarModule),
